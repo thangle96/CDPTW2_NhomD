985006bf22294eab0b22166cb28b1d9c945ec06d
Thắng Lê <thangle96.it@gmail.com>
2020-11-25 10:27:16 +0700

    Update popup.js

--- Extension/autocheck/public/js/popup.js
+++ Extension/autocheck/public/js/popup.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
+	$('#save').click(function () {
+		$.get("http://localhost:80/nnnnn/login/saved.php",function(data){
+		$("#ketqua").html(data);
+		});
+	})
 	$.get("http://localhost:80/nnnnn/login/checkgv.php",function(data){
 		$("#ketqua").html(data);
 	});
+	
 });
