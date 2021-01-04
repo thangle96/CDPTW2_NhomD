65eba642693c2d490834a6584a0d386fcc4e1cf2
Thắng Lê <74522109+thangle96@users.noreply.github.com>
2021-01-04 10:45:35 +0700

    update laravel

---
+++ blog/resources/lang/vi/category-admin.php
@@ -0,0 +1,360 @@
+<?php
+
+return [
+
+    /*
+    |-----------------------------------------------------------------------
+    | MAIN MENU
+    |-----------------------------------------------------------------------
+    | Top menu
+    |
+    */
+    'menus' => [
+        'top-menu' => 'Categories',
+        'top-menu-contexts' => 'Contexts',
+        'category' => 'Categories',
+        'context' => 'Context',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | SIDEBAR
+    |-----------------------------------------------------------------------
+    | Left side bar
+    |
+    |
+    |
+    */
+    'sidebar' => [
+        'list' => 'Items',
+        'add' => 'Add new',
+        'trash' => 'Trash',
+        'config' => 'Configurations',
+        'lang' => 'Languages',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | Table column
+    |-----------------------------------------------------------------------
+    | The list of columns in table
+    |
+    */
+    'columns' => [
+        'any'   => 'Any',
+        'order' => '#',
+        'id'    => 'ID',
+        'category-name' => 'Category name',
+        'user-full-name' => 'User full name',
+        'context-name' => 'Context name',
+        'operations' => 'Operations',
+        'updated_at' => 'Updated at',
+        'filename' => 'File name',
+        'context-ref' => 'Ref',
+        'context-status' => 'Status',
+        'key' => 'Key',
+        'status' => 'Status',
+    ],
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | Pages
+    |-----------------------------------------------------------------------
+    | Pages
+    |
+    */
+    'pages' => [
+        'title-list' => 'List of categories',
+        'title-list-context' => 'List of contexts',
+        'title-list-search' => 'Search results',
+        'title-edit' => 'Edit category',
+        'title-edit-context' => 'Edit context',
+        'title-add' => 'Add new category',
+        'title-add-context' => 'Add new context',
+        'title-delete' => 'Delete category',
+        'title-delete-context' => 'Delete context',
+        'title-config' => 'Current configurations',
+        'title-lang' => 'Manage languages',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | Button
+    |-----------------------------------------------------------------------
+    | The list of buttons
+    |
+    */
+    'buttons' => [
+        'search' => 'Search',
+        'reset' => 'Resest',
+        'add' => 'Add',
+        'save' => 'Save',
+        'delete' => 'Delete',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | Form
+    |-----------------------------------------------------------------------
+    | The list of elements in form
+    |
+    |
+    */
+    'form' => [
+        'keyword' => 'Keyword',
+        'sorting' => 'Sorting',
+        'no-selected' => 'No selected',
+        'status' => 'Status',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | Description
+    |-----------------------------------------------------------------------
+    | Description
+    |
+    */
+    'descriptions' => [
+        'category-form' => 'Category form',
+        'overview' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'description' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'context-form' => 'Context form',
+        'update' => 'Update category',
+        'category-name' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'category' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'list' => 'List of items',
+        'counters' => 'There are <b>:number</b> items',
+        'counter' => 'There is <b>:number</b> item',
+        'not-found' => 'Not found items',
+        'config' => 'List of configurations',
+        'lang' => 'List of languages',
+        'category-slug' =>'Category Slug',
+        'context-name' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'context-ref' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'context-key' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'context-status' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+        'status' => '<p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>',
+    ],
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | Error
+    |-----------------------------------------------------------------------
+    | Show error message
+    |
+    |
+    |
+    */
+    'errors' => [
+        'required' => ':attribute is required',
+        'required_length' => 'Allow from: <b>:minlength</b> to <b>:maxlength</b>. characters',
+    ],
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | FIELDS
+    |-----------------------------------------------------------------------
+    | Column name in table
+    |
+    |
+    |
+    */
+    'fields' => [
+        'id' => 'Category ID',
+        'name' => 'Category name',
+        'name-context' => 'Context name',
+        'description' => 'Description',
+        'overview' => 'Overview',
+        'slug' => 'Slug',
+        'updated_at' => 'Updated at',
+        'context-ref' => 'Ref',
+        'context-name' => 'Name',
+        'context-status' => 'Status',
+
+    ],
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | LABLES
+    |-----------------------------------------------------------------------
+    | The lables of element in form
+    |
+    |
+    |
+    */
+    'labels' => [
+        'category-name' => 'Category name',
+        'overview' => 'Overview',
+        'description' => 'Description',
+        'context-name' => 'Context name',
+        'category' => 'Category name',
+        'context' => 'Context name',
+        'title-search' => 'Search category',
+        'title-search-context' => 'Search contexts',
+        'title-backup' => 'Backups',
+        'config' => 'Configurations',
+
+        'context_name' => 'Context name',
+        'sorting' => 'Sorting',
+        'status' => 'Status',
+
+        'context-ref' => 'References',
+        'context-key' => 'Context key',
+        'context-status' => 'Status',
+        'unknown' => 'Unknown',
+        'category-slug' =>'Category Slug',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | TABS
+    |-----------------------------------------------------------------------
+    | The name of tab
+    |
+    |
+    |
+      */
+    'tabs' => [
+        'menu_1' => 'Basic',
+        'menu_2' => 'Advance',
+        'menu_3' => 'Other',
+        'menu_4' => 'Menu 4',
+        'menu_5' => 'Menu 5',
+        'menu_6' => 'Menu 6',
+        'menu_7' => 'Menu 7',
+        'menu_8' => 'Menu 8',
+        'menu_9' => 'Menu 9',
+        'menu_9' => 'Menu 9',
+        'guide'  => 'Guide',
+        'other'  => 'Other',
+        'basic'  => 'Basic',
+        'advance' => 'Advance',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | HEADING
+    |-----------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    */
+    'headings' => [
+        'form-search' => 'Search categorys',
+        'list' => 'List of categorys',
+        'search' => 'Search results',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | CONFIRMS
+    |-----------------------------------------------------------------------
+    | List of messages for confirm
+    |
+    |
+    |
+    */
+    'confirms' => [
+        'delete' => 'Are you sure you want to delete this item?',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | ACTIONS
+    |-----------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    */
+    'actions' => [
+        'add-ok' => 'Add item successfully',
+        'add-error' => 'Add item failed',
+        'edit-ok' => 'Edit item successfully',
+        'edit-error' => 'Edit item failed',
+        'delete-ok' => 'Delete item successfully',
+        'delete-error' => 'Delete item failed',
+    ],
+
+
+
+
+
+    /*
+    |-----------------------------------------------------------------------
+    | SEARCH
+    |-----------------------------------------------------------------------
+    |
+    |
+    |
+    |
+    */
+    'order' => [
+        'by-asc' => 'ASC',
+        'by-des' => 'DES',
+    ],
+];
---
+++ blog/resources/views/vendor/package-category/admin/partials/input_slug.blade.php
@@ -0,0 +1,83 @@
+<!--
+| @TITLE
+| Input text element in form
+|
+|-------------------------------------------------------------------------------
+| @REQUIRED
+| $name is input name
+| $value is input value
+| $label is input lable
+| $placehover is placehover text
+| $errors is error name
+| $description is description text
+|
+|÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷
+| @SYNTAX
+|
+------------------------------------------------------------------------------->
+
+<!--DATA-->
+<?php
+    //name
+    $name = empty($name)?'undefined':$name;
+    //id
+    $id = empty($id)?$name:$id;
+    //ref
+    $ref = empty($ref)?$name:$ref;
+    //value
+    $value = empty($value)?$request->get($name):$value;
+    //label
+    $label = empty($label) ? '' : $label;
+    //place hover
+    $placehover = empty($placehover) ? $label : $placehover;
+    //eror
+    $errors = empty($errors) ? '' : $errors;
+    //description
+    $description = empty($description) ? '' : $description;
+?>
+<!--/DATA-->
+
+<!-- INPUT TEXT -->
+<div class="form-group">
+
+    <!--element-->
+    {!! Form::label($name, $label) !!}
+    {!! Form::text($name, $value, ['id' => $id, 'class' => 'form-control', 'placeholder' => $placehover]) !!}
+
+    <!--description-->
+    @if($description)
+        <span class='input-text-description'>
+            <blockquote class="quote-card">
+                <p>{!! $description !!}</p>
+            </blockquote>
+        </span>
+    @endif
+
+    <!--errors-->
+    @if ($errors->has($name))
+        <ul class='alert alert-danger error-item'>
+            @foreach($errors->get($name) as $error)
+                @if($error)
+                <li>
+                    <span class='input-text-error'>{!! $error !!}</span>
+                </li>
+                @endif
+            @endforeach
+        </ul>
+    @endif
+</div>
+<!-- /INPUT TEXT -->
+
+<!-- /INPUT IMAGE -->
+@section('footer_scripts')
+    @parent
+    {!! HTML::script('packages/foostart/js/slugit.js') !!}
+
+    <script type='text/javascript'>
+        $(document).ready(function(){
+            $('#<?php echo $ref ?>').slugIt({
+                   output: '#<?php echo $id ?>'
+               });
+        });
+    </script>
+@endsection
